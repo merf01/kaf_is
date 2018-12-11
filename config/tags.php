947a4c3969e895cba45f4e537bd935022ab8a5ed
Sektum <eyemerf@users.noreply.github.com>
2018-12-11 21:01:12 +0300

    Update tags.php

--- config/tags.php
+++
@@ -1,9 +0,0 @@
-
-// function getOptions(){
-//     $type=(isset($_GET['type'])) ? implode($_GET['type'], ',') : null;
-//     $status=(isset($_GET['status']))? implode($_GET['status'], ','):null;
-//
-// return array(
-//   'type'=>$type,
-//   'status'=>$status
-// );
