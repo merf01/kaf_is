bfb4db0e18c10a3428ecfcc549ddd50022a6648b
Sektum <eyemerf@users.noreply.github.com>
2018-12-20 12:39:55 +0300

    .

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,31 +1,22 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Comment;
-
 class CommentController extends Controller
 {
     public function store (Request $request)
     {
       $comment = new Comment();
-
       $comment->project_id = $request->input("project_id");
       $comment->user_id = auth()->user()->id;
       $comment->comment_text = $request->input("comment_text");
-
       	$comment->save();
       return redirect()->back();
-
-  //  return _project($comment->project_id)->comments;
     }
-
     public function destroy($id)
   	{
   		$comment = Comment::findOrFail($id);
   		$comment->delete();
-
   		return redirect()->back();
   	}
 }
